74bf23137517db398f0e820be4bb5380617ad046
HadiAli45624 <hadi.ali45624@gmail.com>
2026-07-23 01:49:01 +0500

    Stage 2: read endpoints with 404

--- Crud_Api/app.py
+++ Crud_Api/app.py
@@ -2,6 +2,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+tasks = [
+    {"id": 1, "title": "Learn Backend", "done": False},
+    {"id": 2, "title": "Complete Data Structure Course", "done": False},
+    {"id": 3, "title": "Get Healthier", "done": True}
+]
+
 @app.get("/")
 def home():
     return jsonify({"name":"Task API", "version": "1.0", "endpoints": ["/tasks"]}),200
@@ -9,6 +15,19 @@ def home():
 @app.get("/health")
 def check():
     return jsonify({"status":"ok"}), 200
+
+@app.get("/tasks")
+def get_tasks():
+    return jsonify(tasks), 200
+
+@app.get("/tasks/<int:id>")
+def get_tasknum(id):
+    for task in tasks:
+        if task["id"] == id:
+            return jsonify(task),200
+    return jsonify({'error': f"Task {id} Not found"}), 404
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
